9be8fca3ba4f87b7714dff0ec9018a72adc8172b
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-20 12:17:46 -0300

    add EditUser

---
+++ src/app/users/EditUser.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { fetchWrapper } from '@/functions/fetch';
+import { useRouter } from 'next/navigation';
+import { SyntheticEvent, useState } from 'react';
+import { AiFillEdit } from 'react-icons/ai';
+
+type UserProps = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export default function EditUser(user: UserProps) {
+  const router = useRouter();
+  const [modal, setModal] = useState(false);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetchWrapper(`users/${user.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    });
+
+    setName('');
+    setEmail('');
+    setModal(false);
+    router.refresh();
+  };
+
+  const handleChange = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <button onClick={handleChange} className="text-gray-900">
+        <AiFillEdit />
+      </button>
+      {modal && (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-full max-w-2xl max-h-full">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5">
+                  <h3 className="text-2xl font-semibold">Edit user</h3>
+                </div>
+
+                <div className="relative p-6 flex-auto">
+                  <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
+                    <div>
+                      <div className="mt-2">
+                        <input
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="Name user"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="mt-2">
+                        <input
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="E-mail"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={handleChange}
+                      >
+                        close
+                      </button>
+                      <button
+                        className="bg-purple-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
+    </>
+  );
+}
