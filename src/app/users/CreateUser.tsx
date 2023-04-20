b3516d824590e7eab0affa6fabab028d0600d5a1
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-20 12:17:28 -0300

    add CreateUser

--- src/app/users/CreateUser.tsx
+++ src/app/users/CreateUser.tsx
@@ -1,11 +1,43 @@
-import { useState } from 'react';
+'use client';
+
+import { fetchWrapper } from '@/functions/fetch';
+import { useRouter } from 'next/navigation';
+import { SyntheticEvent, useState } from 'react';
 
 export default function CreateUser() {
+  const router = useRouter();
   const [modal, setModal] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetchWrapper('users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    });
+
+    setName('');
+    setEmail('');
+    setModal(false);
+    router.refresh();
+  };
+
+  const handleChange = () => {
+    setModal(!modal);
+  };
+
   return (
     <>
       <button
-        onClick={() => setModal(!modal)}
+        onClick={handleChange}
         className="bg-gray-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
       >
         Create user
@@ -20,12 +52,14 @@ export default function CreateUser() {
                 </div>
 
                 <div className="relative p-6 flex-auto">
-                  <form className="space-y-6" action="#" method="POST">
+                  <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
                     <div>
                       <div className="mt-2">
                         <input
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           placeholder="Name user"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
                         />
                       </div>
                     </div>
@@ -34,27 +68,27 @@ export default function CreateUser() {
                         <input
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           placeholder="E-mail"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
                         />
                       </div>
                     </div>
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={handleChange}
+                      >
+                        close
+                      </button>
+                      <button
+                        className="bg-purple-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                      >
+                        Create
+                      </button>
+                    </div>
                   </form>
-                </div>
-
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setModal(!modal)}
-                  >
-                    close
-                  </button>
-                  <button
-                    className="bg-purple-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setModal(!modal)}
-                  >
-                    Create
-                  </button>
                 </div>
               </div>
             </div>
